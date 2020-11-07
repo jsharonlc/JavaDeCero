231c825f67d54a90f027a714be62961230612b0d
jsharon.lc <jsharon.lc@gmail.com>
2020-11-07 02:37:39 +0100

    Se ha agregado un cuestionario y una tarea de resolución

---
+++ src/Cuestionario.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shalc
+ */
+public class Cuestionario {
+    /* ¿Cuál es la salida del siguiente código?
+    var nombre = "Juan";
+    System.out.println(nombre);
+    nombre = "Carlos";
+    System.out.println(nombre);
+    
+    RESPUESTAS
+    a) Juan
+        Juan
+    
+    b) Juan
+        Carlos
+    
+    c) Carlos
+        Carlos
+    
+    d) Error
+    
+    
+    Correcta B*/
+    
+    /*-------------------------------------------*/
+    
+    /* ¿Cuál es la salida del siguiente código?
+    String color;
+    System.out.println(color);
+    color= "Rojo";
+    
+    RESPUESTAS
+    a) Rojo
+    
+    b) El programa se ejecuta correctamente, pero nada se imprime en consola
+    
+    c) Error
+    
+    
+    Correcta C
+    En el momento en que se imprime la variable, no se ha asignado aún un valor
+    a la variable y el compilador manda el erro: variable color might not have been
+    initialized (la variable color tal vez no ha sido inicializada)
+    */
+    
+    
+    
+    /*-------------------------------------------*/
+    
+    /* ¿Qué variable se nombra correctamente?
+    a) String TituloCancion
+    
+    b) String tituloCancion
+    
+    c) String titulocancion
+    
+    
+    Correcta B 
+    La variable se nombra con estilo notación de camillo de altas y bajas
+    */
+    
+    
+    
+    /*-------------------------------------------*/
+    
+    /* ¿Qué variable se nombra correctamente?
+    a) int personaid
+    
+    b) int personaID
+    
+    c) int personaId
+    
+    
+    Correcta C
+    La variable se nombra con estilo notación de camillo de altas y bajas
+    */
+    
+    
+    
+    /*-------------------------------------------*/
+    
+    /* ¿Cuál es la salida del siguiente código?
+    a="A";
+    System.out.println(a);
+    a="B";
+    System.out.println(b);
+    a)  A
+        B
+    
+    b)  A
+        A
+    
+    c) Error
+    
+    
+    Correcta C
+    La variable a no se ha definido, no tiene un tipo definido
+    */
+    
+}
---
+++ src/TareaDetallesLibro.java
@@ -0,0 +1,69 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shalc
+ */
+
+/*titulo
+
+autor
+
+Debes imprimir la información en el siguiente formato:
+
+Proporciona el titulo:
+Proporciona el autor:
+<titulo> fue escrito por <autor>
+Puedes utilizar el IDE de tu preferencia para desarrollar la solución y después pegar aquí tu respuesta, ya que este editor no contiene ningún tipo de ayuda.
+
+Preguntas de esta tarea
+¿Cuál es el código del requerimiento solicitado?*/
+public class TareaDetallesLibro {
+    public static void main(String args[]) {
+        System.out.println("Escribe el titulo:");
+        Scanner consola = new Scanner(System.in);
+        var titulo = consola.nextLine();
+        
+        System.out.println("Escribe el autor:");
+        var autor = consola.nextLine();
+        System.out.println(titulo + " fue escrito por " + autor);
+    }
+}
+
+
+/*SOLUCIÓN
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Escribe tu solucion aqui
+        System.out.println("Proporciona el titulo:");
+        String titulo = scanner.nextLine();
+        System.out.println("Proporciona el autor:");
+        String autor = scanner.nextLine();
+        System.out.println(titulo + " fue escrito por " + autor);
+    }
+
+__________________________________________________________________________
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Escribe tu solucion aqui
+        System.out.println("Proporciona el titulo:");
+        String titulo = scanner.nextLine();
+        System.out.println("Proporciona el autor:");
+        String autor = scanner.nextLine();
+        System.out.println(titulo + " fue escrito por " + autor);
+    }
+}
+}*/

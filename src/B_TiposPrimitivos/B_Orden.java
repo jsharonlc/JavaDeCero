4c85cfd24d2f821d608a3606132f032ca72598f2
jsharon.lc <jsharon.lc@gmail.com>
2020-11-07 21:49:31 +0100

    Índice de Orden

--- src/B_TiposPrimitivos/B_Orden.java
+++ src/B_TiposPrimitivos/B_Orden.java
@@ -10,5 +10,17 @@ package B_TiposPrimitivos;
  * @author Shalc
  */
 public class B_Orden {
+    /* Orden
+    1. Tipos Primitivos en Java
+    2. Tipos Numéricos Enteros
+    3. Ejercicio: Tipos Primitivos tipos Flotantes
+    4. Ejercicio: Inferencia de Tipos con Var y tipos Primitivos
+    5. Ejercicio: Tipos Primitivos tipo char
+    6. Ejercicio: Tipos Primitivos tipo boolean
+    7. Ejercicio: Conversión de Tipos Primitivos en Java - parte 1
+    8. Ejercicio: Conversión de Tipos Primitivos en Java - parte 2
+    9. Cuestionario 2: Preguntas de Números
+    10. Tarea 3: Tienda de Libros y Tienda de Libros (Solución)
+    */
     
 }

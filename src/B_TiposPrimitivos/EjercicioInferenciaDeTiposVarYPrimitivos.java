739b7e407e96062224c25ddc049d24d9f08199dd
jsharon.lc <jsharon.lc@gmail.com>
2020-11-08 02:50:28 +0100

    Aprendizaje en la interferencia de tipos

---
+++ src/B_TiposPrimitivos/EjercicioInferenciaDeTiposVarYPrimitivos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package B_TiposPrimitivos;
+
+/**
+ *
+ * @author Shalc
+ */
+public class EjercicioInferenciaDeTiposVarYPrimitivos {
+    public static void main(String args[]) {
+        var numeroEntero = 10;
+        System.out.println("numeroEntero = " + numeroEntero);
+        
+        var numeroDouble = 10.0;
+        System.out.println("numeroDouble = " + numeroDouble);
+        
+        var numeroFloat = 10.0F;
+        System.out.println("numeroFloat = " + numeroFloat);
+    }
+}
